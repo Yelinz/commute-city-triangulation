54a0bcfcd13c765de18056a28f676051889831c0
yelinz <yelin.zhang@adfinis.com>
2023-10-26 22:17:33 +0200

    add docs

--- App.py
+++ App.py
@@ -6,6 +6,7 @@ import seaborn
 import pydeck
 
 
+# maybe it would be more effective to have the feed as a global variable, instead of passing it in
 @st.cache_data
 def load_feed():
     return gtfs_kit.read_feed("gtfs_fp2024_2023-10-18_04-15.zip", dist_units="km")
@@ -18,6 +19,7 @@ def parse_stations(_feed):
     )
 
 
+@st.cache_data
 def get_routes(_feed, station_id):
     platforms = _feed.stops.loc[_feed.stops["parent_station"] == station_id]
     all_stop_times = _feed.stop_times.loc[
@@ -32,8 +34,49 @@ def get_routes(_feed, station_id):
     return routes
 
 
-def get_trips(_feed, _routes, _excluded_routes):
-    print("treips", _routes)
+@st.cache_data
+def get_stops(_feed, route_ids, weekdays_exclusion, relevant_hours):
+    route_trips = _feed.trips.loc[_feed.trips["route_id"].isin(route_ids)]
+
+    # filter by weekdays
+
+    relevant_stops = _feed.stop_times.loc[_feed.stop_times["trip_id"].isin(route_trips["trip_id"])]
+
+    # parse arrival and departure to timedeltas
+    relevant_stops.loc[:, "arrival_time_parsed"] = pd.to_timedelta(
+        relevant_stops["arrival_time"]
+    )
+    relevant_stops.loc[:, "departure_time_parsed"] = pd.to_timedelta(
+        relevant_stops["departure_time"]
+    )
+
+    # pickup, dropoff type not 0 means no normal passenger transfer
+    lower_bound, upper_bound = [pd.Timedelta(x, unit="h") for x in relevant_hours]
+    filtered_stops = relevant_stops.loc[
+        ((relevant_stops["pickup_type"] == 0) & (relevant_stops["drop_off_type"] == 0))
+        & (
+            (
+                (relevant_stops["arrival_time_parsed"] > lower_bound)
+                & (relevant_stops["arrival_time_parsed"] < upper_bound)
+            )
+            | (
+                (relevant_stops["departure_time_parsed"] > lower_bound)
+                & (relevant_stops["departure_time_parsed"] < upper_bound)
+            )
+        )
+    ]
+
+    return filtered_stops
+
+
+
+
+def get_trips(_feed, _routes, _excluded_routes, _weekdays_exclusion, _relevant_hours):
+    # TODO modularize this mess
+    # filter out excluded routes
+    if _excluded_routes is not None:
+        _routes = _routes.loc[~_routes["route_id"].isin(_excluded_routes)]
+
     route_trips = _feed.trips.loc[_feed.trips["route_id"].isin(_routes["route_id"])]
 
     # all stops of the trips
@@ -50,18 +93,18 @@ def get_trips(_feed, _routes, _excluded_routes):
         relevant_stops["departure_time"]
     )
 
-    # limit arrival and departure to "normal" times 06-22
     # pickup, dropoff type not 0 means no normal passenger transfer
+    lower_bound, upper_bound = [pd.Timedelta(x, unit="h") for x in _relevant_hours]
     filtered_stops = relevant_stops.loc[
         ((relevant_stops["pickup_type"] == 0) & (relevant_stops["drop_off_type"] == 0))
         & (
             (
-                (relevant_stops["arrival_time_parsed"] > pd.Timedelta(6, unit="h"))
-                & (relevant_stops["arrival_time_parsed"] < pd.Timedelta(22, unit="h"))
+                (relevant_stops["arrival_time_parsed"] > lower_bound)
+                & (relevant_stops["arrival_time_parsed"] < upper_bound)
             )
             | (
-                (relevant_stops["departure_time_parsed"] > pd.Timedelta(6, unit="h"))
-                & (relevant_stops["departure_time_parsed"] < pd.Timedelta(22, unit="h"))
+                (relevant_stops["departure_time_parsed"] > lower_bound)
+                & (relevant_stops["departure_time_parsed"] < upper_bound)
             )
         )
     ]
@@ -87,28 +130,41 @@ def get_trips(_feed, _routes, _excluded_routes):
     return stop_data
 
 
+# def draw_stations(input_stations, *station_dfs):
 def draw_stations(stations):
+    shown_stops = stations.drop_duplicates(subset=["parent_station"], keep='first')
+
+    scatter = []
+    for stop_index, stop in shown_stops.iterrows():
+        scatter.append(
+            {
+                "name": stop["stop_name"],
+                "position": stop[["stop_lon", "stop_lat"]].to_list(),
+            }
+        )
+    
     return pydeck.Layer(
         type="ScatterplotLayer",
-        data=stations,
+        data=scatter,
         pickable=True,
-        opacity=0.8,
         stroked=True,
         filled=True,
         radius_scale=6,
         radius_min_pixels=1,
         radius_max_pixels=100,
         line_width_min_pixels=1,
-        get_position="positiion",
-        get_radius=5,
+        line_width_max_pixels=1,
+        get_radius=15,
+        # TODO: different color for each route
+        get_color=[255, 140, 0],
     )
 
 
 def draw_routes(trip_data):
     grouped = trip_data.sort_values(["trip_id", "stop_sequence"]).groupby("trip_id")
+    # TODO: different palette for each route
     colors = seaborn.color_palette("viridis", n_colors=grouped.ngroups)
     idx = 0
-    skipped_routes = []
     route_segments = []
     for name, group in grouped:
         destination_stop_in_route = False
@@ -117,33 +173,19 @@ def draw_routes(trip_data):
 
         for row_index, row in group.iterrows():
             stop_coords.append(row[["stop_lon", "stop_lat"]].to_list())
-
             # tooltip_content += f"<br/>{row['stop_name']}"
-
-            # remove leftover markers or pre condition it
-            # if (row["parent_station"] == station["stop_id"]).bool():
-            #     destination_stop_in_route = True
 
         route_segments.append(
             {
-                "name": name,
+                "name": tooltip_content,
                 "color": [round(n * 255) for n in colors[idx]],
                 "path": stop_coords,
             }
         )
         idx += 1
 
-        # hide a few more routes
-        # if destination_stop_in_route:
-        # TODO check if there are duplicates, if there are render them side by side
-        #     folium.PolyLine(stop_coords, tooltip=tooltip_content, color=colors[idx]).add_to(path_layer)
-        #     idx += 1
-        # else:
-        #     skipped_routes.append(name)
-
     return pydeck.Layer(
         type="PathLayer",
-        # data="https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-lines.json",
         data=route_segments,
         pickable=True,
         width_scale=20,
@@ -161,12 +203,13 @@ st.header("Direct train connections")
 feed = load_feed()
 stations = parse_stations(feed)
 
+
 ## Filters
 #####
 filter_container = st.container()
 filter_col1, filter_col2 = filter_container.columns(2)
 
-# TODO: maybe have an arbitrary amount of selection? filter out selected from other select?
+# restricted to two for now
 selected_city_a = filter_col1.selectbox(
     "City A",
     stations["stop_id"],
@@ -176,11 +219,11 @@ selected_city_a = filter_col1.selectbox(
 # selected_city_b = filter_col1.selectbox("City B", stations["stop_name"], None)
 
 routes_a = get_routes(feed, selected_city_a)
-routes = routes_a
+all_routes = routes_a
 route_exclusion = filter_col2.multiselect(
     "Exclude lines (optional)",
-    routes["route_id"],
-    format_func=lambda id: routes.loc[routes["route_id"] == id][
+    all_routes["route_id"],
+    format_func=lambda id: all_routes.loc[all_routes["route_id"] == id][
         "route_short_name"
     ].iloc[0],
 )
@@ -189,12 +232,19 @@ weekdays_exclusion = filter_col2.multiselect(
     ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     ["Saturday", "Sunday"],
 )
+relevant_hours = filter_col2.slider(
+    "Relevant hours",
+    0, 23, (6, 22), 1
+)
 
 # if selected_city_a is not None and selected_city_a == selected_city_b:
 #    st.error("Same cities selected")
 
 
-trips_a = get_trips(feed, routes_a, route_exclusion)
+filtered_routes = all_routes.loc[~all_routes["route_id"].isin(route_exclusion)]
+stops = get_stops(feed, filtered_routes["route_id"], weekdays_exclusion, relevant_hours)
+print(stops)
+trips_a = get_trips(feed, routes_a, route_exclusion, weekdays_exclusion, relevant_hours)
 
 st.markdown("""---""")
 ## Content
@@ -207,12 +257,12 @@ with map_col:
         latitude=46.848, longitude=8.1336, zoom=7  # Geographical center of switzerland
     )
     route_layer = draw_routes(trips_a)
-    # station_layer = draw_stations()
+    station_layer = draw_stations(trips_a)
 
-    # some weird caching behaviour in here
     deck = pydeck.Deck(
-        layers=[route_layer],
+        layers=[route_layer, station_layer],
         initial_view_state=view_state,
+        tooltip={"text": "{name}"},
     )
     st.pydeck_chart(
         deck,
@@ -222,9 +272,3 @@ with map_col:
 with other_col:
     # bar chart with time from destination to other stations, which filters on click?
     pass
-
-"""
-Please double check any connection with the real SBB timetable. This tool is only as accurate as the provided data.
-Some stops in some routes are only served on special times (Weekend, Rush hour).
-Others are faulty data (Train depot) they do not appear in the real timetable.
-"""
---
+++ pages/Documentation.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown("""
+# Documentation
+
+This app is a tool to find direct train connections between two cities in Switzerland.
+
+Disclaimer:
+Please double check any connection with the real SBB timetable. This tool is only as accurate as the provided data.
+Some stops in some routes are only served on special times (Weekend, Rush hour).
+Others are faulty data (Train depot) they do not appear in the real timetable.
+
+## Filters
+
+""")

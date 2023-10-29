392737ccc72483ba99664c8ccde061d91b3da995
Yelin Zhang <y3lin.zhang@gmail.com>
2023-10-29 19:13:09 +0100

    abandon streamlit

--- streamlit/App.py
+++ streamlit/App.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
 import gtfs_kit
-import folium
 import seaborn
 import pydeck
+
+
+#
+# FIXME: Steamlit version has to be abandoned as there is no good interactivity.
+# https://discuss.streamlit.io/t/is-pydeck-chart-click-interaction-possible/49965/2
+#
 
 
 # maybe it would be more effective to have the feed as a global variable, instead of passing it in
@@ -35,10 +40,16 @@ def get_routes(_feed, station_id):
 
 
 @st.cache_data
-def get_stops(_feed, route_ids, weekdays_exclusion, relevant_hours):
-    route_trips = _feed.trips.loc[_feed.trips["route_id"].isin(route_ids)]
-
+def get_stops(_feed, route_ids, active_days, relevant_hours):
     # filter by weekdays
+    query_string = ""
+    for i, weekday in enumerate(active_days):
+        query_string += f"{weekday.lower()} == 1"
+        if i < len(active_days) - 1:
+            query_string += " and "
+    active_services = _feed.calendar.query(query_string)["service_id"]
+
+    route_trips = _feed.trips.loc[_feed.trips["route_id"].isin(route_ids) & _feed.trips["service_id"].isin(active_services)]
 
     relevant_stops = _feed.stop_times.loc[_feed.stop_times["trip_id"].isin(route_trips["trip_id"])]
 
@@ -51,6 +62,7 @@ def get_stops(_feed, route_ids, weekdays_exclusion, relevant_hours):
     )
 
     # pickup, dropoff type not 0 means no normal passenger transfer
+    # filter by arrival and departure time
     lower_bound, upper_bound = [pd.Timedelta(x, unit="h") for x in relevant_hours]
     filtered_stops = relevant_stops.loc[
         ((relevant_stops["pickup_type"] == 0) & (relevant_stops["drop_off_type"] == 0))
@@ -66,7 +78,26 @@ def get_stops(_feed, route_ids, weekdays_exclusion, relevant_hours):
         )
     ]
 
-    return filtered_stops
+    stops_trips = pd.merge(filtered_stops, _feed.trips[["trip_id", "route_id"]], on="trip_id", how="left")
+    # map the stops to the route
+    stops_route = pd.merge(stops_trips, _feed.routes[["route_id", "route_short_name"]], on="route_id", how="left")
+
+    # only use longest trips
+    longest_trips = stops_route.loc[stops_route.groupby(["route_id"])["stop_sequence"].idxmax()]
+    # stops_to_display = stops_route.loc[stops_route["trip_id"].isin(longest_trips["trip_id"])]
+
+    longest_trips_stop_times = _feed.stop_times.loc[_feed.stop_times["trip_id"].isin(longest_trips["trip_id"])]
+    longest_trips_stations = _feed.stops.loc[_feed.stops["stop_id"].isin(longest_trips_stop_times["stop_id"])]
+    # TODO: consider all trip options, as some might have divergent routes
+    # dedupe stops per route
+    #deduped_stops = stops.sort_values(["stop_sequence"]).groupby("route_id").apply(lambda x: x.loc[x["stop_sequence"].idxmax()])
+    #print(deduped_stops)
+
+    # add all additional data which is needed
+    stop_data = pd.merge(longest_trips_stop_times, _feed.trips, on="trip_id")
+    stop_data = pd.merge(stop_data, _feed.routes, on="route_id")
+    stop_data = pd.merge(stop_data, _feed.stops, on="stop_id")
+    return stop_data
 
 
 
@@ -132,10 +163,11 @@ def get_trips(_feed, _routes, _excluded_routes, _weekdays_exclusion, _relevant_h
 
 # def draw_stations(input_stations, *station_dfs):
 def draw_stations(stations):
-    shown_stops = stations.drop_duplicates(subset=["parent_station"], keep='first')
+
+    #shown_stops = stations.drop_duplicates(subset=["parent_station"], keep='first')
 
     scatter = []
-    for stop_index, stop in shown_stops.iterrows():
+    for stop_index, stop in stations.iterrows():
         scatter.append(
             {
                 "name": stop["stop_name"],
@@ -161,13 +193,12 @@ def draw_stations(stations):
 
 
 def draw_routes(trip_data):
-    grouped = trip_data.sort_values(["trip_id", "stop_sequence"]).groupby("trip_id")
+    grouped = trip_data.sort_values(["route_id", "stop_sequence"]).groupby("route_id")
     # TODO: different palette for each route
     colors = seaborn.color_palette("viridis", n_colors=grouped.ngroups)
     idx = 0
     route_segments = []
     for name, group in grouped:
-        destination_stop_in_route = False
         stop_coords = []
         tooltip_content = f"{group.iloc[0]['route_short_name']}"
 
@@ -227,10 +258,10 @@ route_exclusion = filter_col2.multiselect(
         "route_short_name"
     ].iloc[0],
 )
-weekdays_exclusion = filter_col2.multiselect(
-    "Weekday exclusion (optional)",
+active_weekdays = filter_col2.multiselect(
+    "Active weekdays",
     ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    ["Saturday", "Sunday"],
+    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
 )
 relevant_hours = filter_col2.slider(
     "Relevant hours",
@@ -242,33 +273,41 @@ relevant_hours = filter_col2.slider(
 
 
 filtered_routes = all_routes.loc[~all_routes["route_id"].isin(route_exclusion)]
-stops = get_stops(feed, filtered_routes["route_id"], weekdays_exclusion, relevant_hours)
-print(stops)
-trips_a = get_trips(feed, routes_a, route_exclusion, weekdays_exclusion, relevant_hours)
+stops_a = get_stops(feed, filtered_routes["route_id"], active_weekdays, relevant_hours)
 
 st.markdown("""---""")
 ## Content
 #####
-content_container = st.container()
-map_col, other_col = content_container.columns([2, 1])
+map_container = st.container()
+map_col, map_legend_col = map_container.columns([2, 1])
 
 with map_col:
     view_state = pydeck.ViewState(
         latitude=46.848, longitude=8.1336, zoom=7  # Geographical center of switzerland
     )
-    route_layer = draw_routes(trips_a)
-    station_layer = draw_stations(trips_a)
+    route_layer = draw_routes(stops_a)
+    station_layer = draw_stations(stops_a)
 
     deck = pydeck.Deck(
         layers=[route_layer, station_layer],
         initial_view_state=view_state,
         tooltip={"text": "{name}"},
     )
-    st.pydeck_chart(
+    print(deck)
+    deck.on_click(lambda x: print(x))
+    re = st.pydeck_chart(
         deck,
         True,
     )
+    print(re)
 
-with other_col:
-    # bar chart with time from destination to other stations, which filters on click?
+with map_legend_col:
+    # legend for route colors, city reachable colors
+    pass
+
+
+station_distance_container = st.container()
+with station_distance_container:
+    # after selected route
+    # display every station and how long it takes to get from each one
     pass
